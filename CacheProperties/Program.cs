9d97b9dc2c40160b1b4d88bb59cc290bcf67eb17
Hristo Hristov <koredalin@gmail.com>
2019-07-21 09:04:42 +0300

    Cache Properties Demo Completed

--- CacheProperties/Program.cs
+++ CacheProperties/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CacheProperties.Estimations;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 
 namespace CacheProperties
@@ -13,10 +14,17 @@ namespace CacheProperties
             feObj.CalculateAll();
             feObj.ClearNotZeroValues();
 
-            string fedEstimationsJson = JsonConvert.SerializeObject(feObj);
-            string fedEstimationsZeroPropertiesValuesJson = JsonConvert.SerializeObject(FedEstimation.ZeroValues);
+            string fedEstimationsJson = JsonConvert.SerializeObject(feObj,
+                Formatting.Indented,
+                new JsonConverter[] { new StringEnumConverter() });
+            string fedEstimationsZeroPropertiesValuesJson = JsonConvert.SerializeObject(FedEstimation.ZeroValues,
+                Formatting.Indented,
+                new JsonConverter[] { new StringEnumConverter() });
 
-            Console.WriteLine(feObj.FedOutA2);
+            Console.WriteLine("Object Properties");
+            Console.WriteLine(fedEstimationsJson);
+            Console.WriteLine("Object Zero Values");
+            Console.WriteLine(fedEstimationsZeroPropertiesValuesJson);
         }
     }
 }
